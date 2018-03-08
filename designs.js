e57d3731cf81d22dd2f0a2b5ab4555ea7ef842dd
Wilson Silva <wilsonsilva80@gmail.com>
2018-03-08 21:02:07 +0000

    Create grid

--- designs.js
+++ designs.js
@@ -4,7 +4,24 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
-
+    //initializations
+    const grid = document.getElementById('pixelCanvas');
+    const height = document.getElementById('inputHeight').value;
+    const width = document.getElementById('inputWeight').value;
+    // const fragment = document.createDocumentFragment();
+    //create rows
+    for(let i = 0; i < height; i++){
+        const row = document.createElement('tr'); //new element
+        grid.appendChild(row);
+        //create columns
+        for(let j = 0; j < width; j++){
+            const col = document.createElement('td');
+            row.appendChild(col);
+        }
+    }
 }
+const submitButton = document.getElementById('gridSubmit');
+submitButton.addEventListener('click', function(evt){
+    evt.preventDefault();
+    makeGrid();
+});
